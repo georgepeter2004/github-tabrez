0a450dd6fdbac3328ab78de2eef2cd93c2294e11
georgepeter2004 <georgepeter200424@gmail.com>
2026-09-21 00:15:21 +0530

    area,main,swiggy

--- src/Main.java
+++ src/Main.java
@@ -2,49 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        boolean flag = false;
-        do {
-            System.out.println("1.circle\n2.rectangle\n3.triangle\n4.Exit\n");
-            flag = true;
-            int ch = sc.nextInt();
 
-            switch (ch){
-                case 1:
-                    System.out.print("Enter Radius: ");
-                    int r = sc.nextInt();
-                    System.out.println("\nArea of Circle: "+ circle((double)r));
-                    break;
-                case 2:
-                    System.out.print("\nEnter Breadth: ");
-                    int b = sc.nextInt();
-                    System.out.print("\nEnter Width: ");
-                    int l = sc.nextInt();
-                    System.out.println("\nArea of Rectangle: "+ rectangle(l,b));
-                    break;
-                case 3:
-                    System.out.print("Enter Breadth: ");
-                    int bb = sc.nextInt();
-                    System.out.print("\nEnter Height: ");
-                    int h = sc.nextInt();
-                    System.out.println("\nArea of Rectangle: "+ triangle(bb,h));
-                    break;
-                case 4:
-                    flag = false;
-            }
-        }while(flag);
-
-    }
-
-    static double circle(double r){
-        return (Math.PI*(r*r));
-    }
-
-    static int rectangle(int l ,int b){
-        return l*b;
-    }
-
-    static int triangle(int b,int h){
-        return (b*h)/2;
+        System.out.println();
     }
 }
--- src/Tabrezclass/Swiggy.java
+++ src/Tabrezclass/Swiggy.java
@@ -2,9 +2,100 @@ package Tabrezclass;
 
 import java.util.Scanner;
 
-public class Swiggy {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+ class Swiggy {
+     static int db_uid;
+     static int db_pwd;
+     static long db_contact;
+     static boolean acc_exists;
+     static Scanner sc = new Scanner(System.in);
+     static long OTP;
+    public static void main(String[] args) throws Exception {
+
+        boolean flag = false;
+        do{
+            System.out.println("\t\t\t-----SWIGGY----");
+            System.out.println("1.SignUp\n2.Login\n3.Forget Password\n4.Logout");
+            int ch = sc.nextInt();
+            flag = true;
+            switch(ch){
+                case 1:
+                    if(!acc_exists){
+                        System.out.print("Enter UID: ");
+                        db_uid = sc.nextInt();
+                        System.out.print("\nEnter pwd: ");
+                        db_pwd = sc.nextInt();
+                        System.out.print("\nEnter your Contact no: ");
+                        db_contact = sc.nextLong();
+                        acc_exists = true;
+                        break;
+                    }
+                    else {
+                        System.out.println("Account is Already exists");
+                        break;
+                    }
+                case 2:
+                    if(acc_exists){
+                        if(login()) {
+                            break;
+                        }
+                        else {
+                            System.out.println("Login failed");
+                            break;
+                        }
+                    }
+                    else{
+                        System.out.println("Create Account to Login");
+                    }
+
+                case 3:
+                  if(acc_exists) { forgetPwd(); break;}
+                  else {System.out.println("Create a Account to use"); break;}
+
+                case 4: flag = false;
+
+            }
+        }while (flag);
+
+    }
+
+    static boolean login(){
+        if(acc_exists){
+            System.out.print("Enter uid: ");
+            int uid = sc.nextInt();
+            System.out.println("\nEnter pwd: ");
+            int pwd = sc.nextInt();
+            if(uid == db_uid && pwd==db_pwd ){
+                System.out.println("Login Successful");
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    static void forgetPwd() throws Exception{
+        if(acc_exists){
+        System.out.print("Enter your Contactno: ");
+        long contact = sc.nextLong();
+        if(contact == db_contact){
+            System.out.println("Verification is successful ");
+            OTP = (long)(Math.random()*9999)/2;
+            Thread.sleep(2000);
+            System.out.println("OTP Received: "+OTP);
+            System.out.println("Enter OTP: ");
+            int otp = sc.nextInt();
+
+            if(otp == OTP){
+                System.out.print("Enter the pwd: ");
+                db_pwd = sc.nextInt();
+                System.out.println("Password Changed");
+                return;
+            }
+            else {
+                System.out.println("OTP Failed...");
+                return;
+            }
+        }}
 
     }
 }
